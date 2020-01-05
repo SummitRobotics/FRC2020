d20a51a3e831901775a52fe84845f8bb8f2c17d3
django <django@inventobot.com>
2020-01-04 22:10:24 -0800

    added javadoc comments to oi

--- src/main/java/frc/robot/oi/OI.java
+++ src/main/java/frc/robot/oi/OI.java
@@ -11,10 +11,16 @@ import frc.robot.utilities.Constants.LoggerRelations;
 public class OI implements Logger{
     XboxController controller = new XboxController(Constants.XBOX_PORT);
 
-    //array of all controler inputs
+    //array of all controller inputs
     double[] valuesToLog = new double[18];
 
-    //wpi compatable butttons
+    //wpi compatible buttons
+
+    //buttons
+
+    /**
+     * A button on the xbox controller
+     */
     public Button buttonA = new Button(){
         @Override
         public boolean get(){
@@ -22,6 +28,9 @@ public class OI implements Logger{
         }                                                                                    
     };
 
+    /**
+     * B button on the xbox controller
+     */
     public Button buttonB = new Button(){
         @Override
         public boolean get(){
@@ -29,6 +38,9 @@ public class OI implements Logger{
         }                                                                                             
     };
 
+    /**
+     * X button on the xbox controller
+     */
     public Button buttonX = new Button(){
         @Override
         public boolean get() {
@@ -36,6 +48,9 @@ public class OI implements Logger{
         }
     };
 
+    /**
+     * Y button on the xbox controller
+     */
     public Button buttonY = new Button(){
         @Override
         public boolean get() {
@@ -43,6 +58,9 @@ public class OI implements Logger{
         }                                                                             
     };
 
+    /**
+     * Start button on the xbox controller
+     */
     public Button buttonStart = new Button(){
         @Override
         public boolean get() {
@@ -50,6 +68,9 @@ public class OI implements Logger{
         }
     };
 
+    /**
+     * Back button on the xbox controller
+     */
     public Button buttonBack = new Button(){
         @Override
         public boolean get() {
@@ -57,7 +78,12 @@ public class OI implements Logger{
         }
     };
 
+
     //dpad - angle gets what buttons are pressed in a stupid way so that is un-done
+
+    /**
+     * checks if D-pad up is pressed
+     */
     public Button dpadUp = new Button(){
         @Override
         public boolean get() {
@@ -68,6 +94,9 @@ public class OI implements Logger{
         }                                                                              
     };
 
+    /**
+     * checks if D-pad down is pressed
+     */
     public Button dpadDown = new Button(){
         @Override
         public boolean get() {
@@ -78,6 +107,9 @@ public class OI implements Logger{
         }                                                                                        
     };
 
+    /**
+     * checks if D-pad left is pressed
+     */
     public Button dpadLeft = new Button(){
         @Override
         public boolean get() {
@@ -88,6 +120,9 @@ public class OI implements Logger{
         }                                                                                    
     };
 
+    /**
+     * checks if D-pad right is pressed
+     */
     public Button dpadRight = new Button(){
         @Override
         public boolean get() {
@@ -98,7 +133,12 @@ public class OI implements Logger{
         }                                                                                       
     };
 
+
     //bumpers
+
+    /**
+     * checks if the right bumper is pressed
+     */
     public Button rightBumper = new Button(){
         @Override
         public boolean get() {
@@ -106,6 +146,9 @@ public class OI implements Logger{
         }
     };
 
+    /**
+     * checks if the left bumper is pressed
+     */
     public Button leftBumper = new Button(){
         @Override
         public boolean get() {
@@ -113,47 +156,81 @@ public class OI implements Logger{
         }
     };
 
-    public Button leftBumperInverted = new Button(){
-        @Override
-        public boolean get() {
-        return !LogPassBool(LoggerRelations.LEFT_BUMPER, controller.getBumper(Hand.kLeft));
-        }
-    };
 
-    //trigers
-    public double rightTriger(){
+    //triggers
+
+    /**
+     * gets right trigger pressed amount
+     * @return 0 - 1
+     */
+    public double rightTrigger(){
         return LogPassDouble(LoggerRelations.RIGHT_TRIGGER, controller.getTriggerAxis(Hand.kRight));
     }
 
-    public double leftTriger(){
+    /**
+     * gets left trigger pressed amount
+     * @return 0 - 1
+     */
+    public double leftTrigger(){
         return LogPassDouble(LoggerRelations.LEFT_TRIGGER, controller.getTriggerAxis(Hand.kLeft));
     }
 
+
     //sticks - x is left and right and y is up and down
+
+    /**
+     * gets the left stick x value
+     * @return -1 - 1
+     */
     public double left_Stick_X(){
         return LogPassDouble(LoggerRelations.LEFT_STICK_X, controller.getX(Hand.kLeft));
     }
 
+    /**
+     * gets the left stick y value
+     * @return -1 - 1
+     */
     public double left_Stick_Y(){
         return LogPassDouble(LoggerRelations.LEFT_STICK_Y, controller.getY(Hand.kLeft));
     }
 
+    /**
+     * gets the right stick x value
+     * @return -1 - 1
+     */
     public double right_Stick_X(){
         return LogPassDouble(LoggerRelations.RIGHT_STICK_X, controller.getX(Hand.kRight));
     }
 
+    /**
+     * gets the right stick y value
+     * @return -1 - 1
+     */
     public double right_Stick_Y(){
         return LogPassDouble(LoggerRelations.RIGHT_STICK_Y, controller.getY(Hand.kRight));
     }
 
+
     //functions to make logging simple
 
+    /**
+     * takes in a boolean and puts it into the valuesToLog array
+     * @param index the LoggerRelations index of the pice of data
+     * @param in the boolean to log
+     * @return the boolean passed in
+     */
     private boolean LogPassBool(LoggerRelations index, boolean in){
         //converts in to double and puts it into the array
         valuesToLog[index.value] = (in? 1.0 : 0.0);
         return in;
     }
 
+    /**
+     * takes in a double and puts it into the valuesToLog array
+     * @param index the LoggerRelations index of the pice of data
+     * @param in the double to log
+     * @return the double passed in
+     */
     private double LogPassDouble(LoggerRelations index, double in){
         //puts double into the array
         valuesToLog[index.value] = in;
@@ -163,7 +240,7 @@ public class OI implements Logger{
     //logging function
     @Override
     public double[] getValues(double[] values) {
-        //runs through the values to log array and sets values eaqual to it
+        //runs through the values to log array and sets values equal to it
         for(int i = 0; i<valuesToLog.length; i++){
             values[i] = valuesToLog[i];
         }
