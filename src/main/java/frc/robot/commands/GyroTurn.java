01519892305b5aaa10b8cb494d6a8286e917c138
Owen <owen356wh@gmail.com>
2020-01-13 17:41:52 -0800

    maybe worthless nonsense

--- src/main/java/frc/robot/commands/GyroTurn.java
+++ src/main/java/frc/robot/commands/GyroTurn.java
@@ -9,7 +9,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-import edu.wpi.first.wpilibj.controller.PIDController;
 import frc.robot.devices.PigeonGyro;
 import frc.robot.livepid.LivePIDController;
 import frc.robot.subsystems.Drivetrain;
--- src/main/java/frc/robot/livepid/LivePIDController.java
+++ src/main/java/frc/robot/livepid/LivePIDController.java
@@ -1,30 +1,41 @@
 package frc.robot.livepid;
 
+import java.util.HashMap;
+
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
 
 public class LivePIDController extends PIDController {
 
-    String name;
-    double defaultP, defaultI, defaultD;
-    NetworkTableEntry P, I, D;
+    private static HashMap<String, NetworkTableEntry[]> PIDNetworkEntries = new HashMap<>();
+
+    private double defaultP, defaultI, defaultD;
+    private NetworkTableEntry[] PID;
 
     public LivePIDController(String name, double defaultP, double defaultI, double defaultD) {
         super(
             defaultP, 
-            defaultI, 
+            defaultI,
             defaultD
         );
 
-        this.name = name;
+        ShuffleboardLayout layout = Shuffleboard.getTab("PID Tuning").getLayout(name, BuiltInLayouts.kList);
+        if (PIDNetworkEntries.containsKey(name)) {
+            PID = PIDNetworkEntries.get(name);
 
-        ShuffleboardLayout layout = Shuffleboard.getTab("PID Tuning").getLayout(name);
-        P = layout.add("P", defaultP).withWidget(BuiltInWidgets.kNumberSlider).getEntry();
-        I = layout.add("I", defaultI).withWidget(BuiltInWidgets.kNumberSlider).getEntry();
-        D = layout.add("D", defaultD).withWidget(BuiltInWidgets.kNumberSlider).getEntry();
+        } else {
+            PID = new NetworkTableEntry[3];
+
+            PID[0] = layout.add("P", defaultP).withWidget(BuiltInWidgets.kNumberSlider).getEntry();
+            PID[1] = layout.add("I", defaultI).withWidget(BuiltInWidgets.kNumberSlider).getEntry();
+            PID[2] = layout.add("D", defaultD).withWidget(BuiltInWidgets.kNumberSlider).getEntry();
+
+            PIDNetworkEntries.put(name, PID);
+        }
 
         this.defaultP = defaultP;
         this.defaultI = defaultI;
@@ -33,9 +44,9 @@ public class LivePIDController extends PIDController {
 
     public void update() {
         setPID(
-            P.getDouble(defaultP), 
-            I.getDouble(defaultI), 
-            D.getDouble(defaultD)
+            PID[0].getDouble(defaultP), 
+            PID[1].getDouble(defaultI), 
+            PID[2].getDouble(defaultD)
         );
     }
 }
