9df556153040cb3dcc113278d1caa675c6f37cc7
Owen <owen356wh@gmail.com>
2020-01-25 14:47:42 -0800

    added functionality

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -2,32 +2,61 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.ControlType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.logging.Logger;
 import frc.robot.logging.LoggerRelations;
 import frc.robot.utilities.Functions;
 import frc.robot.utilities.Ports;
 
-public class Turret implements Logger{
+public class Turret extends SubsystemBase implements Logger {
 
-    TalonSRX turret = new TalonSRX(Ports.TURRET.port);
     double oldPower = 0;
 
-    public Turret(){
-        turret.configPeakOutputForward(1);
-        turret.configPeakOutputReverse(-1);
-        //limits motor power so we dont break stuff
+    CANSparkMax turret;
+    CANEncoder encoder;
+    CANPIDController pidController;
+
+    //TODO - Fix PID and FF values
+    private static final double
+    P = 0,
+    I = 0,
+    D = 0,
+    FF = 0;
+
+    public Turret() {
+        turret = new CANSparkMax(Ports.TURRET.port, MotorType.kBrushless);
+        encoder = turret.getEncoder();
+        pidController = turret.getPIDController();
+
+        turret.setClosedLoopRampRate(0);
+
+        pidController.setP(P);
+        pidController.setI(I);
+        pidController.setD(D);
+        pidController.setFF(FF);
+        pidController.setOutputRange(-1, 1);
+
         turret.setInverted(true);
     }
 
     public void setPower(double power){
         oldPower = power;
         power = Functions.clampDouble(power, 1, -1);
-        turret.set(ControlMode.PercentOutput, power);
+        turret.set(power);
+    }
+
+    public void setTarget(double value) {
+        pidController.setReference(value, ControlType.kPosition);
     }
 
     public void stop(){
-        turret.set(ControlMode.PercentOutput, 0);
+        turret.set(0);
     }
 
     @Override
