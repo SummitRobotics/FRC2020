09165ec72d7173b5d30d769e0feb217b4444056a
Django <django@inventobot.com>
2020-01-31 15:41:08 -0800

    causing django pain pt 1
    agggoooonnnyyy

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,18 +2,13 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import frc.robot.logging.SyncLogger;
 import frc.robot.oi.ControllerDriver;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Shifter;
 import frc.robot.utilities.Ports;
-import frc.robot.subsystems.Turret;
-import frc.robot.utilities.powerlimiting.PowerLimiter;
 import frc.robot.commandgroups.AppeaseDuane;
 import frc.robot.commands.*;
-import frc.robot.devices.Lemonlight;
-import frc.robot.devices.PDP;
 import frc.robot.devices.PigeonGyro;
 
 /**
@@ -35,11 +30,6 @@ public class RobotContainer {
   private Drivetrain drivetrain;
   private Shifter shifter;
 
-  private Lemonlight limelight;
-
-  private Turret turret;
-  private PowerLimiter powerLimiter;
-  private PDP pdp;
   /**
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
@@ -52,15 +42,7 @@ public class RobotContainer {
     gyro = new PigeonGyro(Ports.PIGEON_IMU.port);
     drivetrain = new Drivetrain();
     shifter = new Shifter();
-
-    limelight = new Lemonlight();
-
-    turret = new Turret();
-
-    pdp = new PDP();
-    powerLimiter = new PowerLimiter(pdp, drivetrain);
     
-    scheduler.setDefaultCommand(powerLimiter, powerLimiter);
     scheduler.setDefaultCommand(drivetrain, new ArcadeDrive(drivetrain, controller1, shifter));
 
     logger.addElements(drivetrain, gyro, shifter);
@@ -76,15 +58,9 @@ public class RobotContainer {
    * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    controller1.buttonB.toggleWhenPressed(new VisionTrack(limelight, turret));
     controller1.leftBumper.whenPressed(new Shift(shifter));
     controller1.dPadRight.whenPressed(new GyroTurn(gyro, drivetrain, 90));
     controller1.dPadLeft.whenPressed(new GyroTurn(gyro, drivetrain, -90));
-
-    controller1.buttonA.toggleWhenPressed(new StartEndCommand(
-      () -> limelight.setLEDMode(Lemonlight.LEDModes.FORCE_ON),
-      () -> limelight.setLEDMode(Lemonlight.LEDModes.FORCE_OFF)
-    ));
   }
 
   /**
