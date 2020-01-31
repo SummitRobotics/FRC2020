aaa74f36d93f5da4598984fb4cb7e51f8fbda4e8
django <django@inventobot.com>
2020-01-30 19:42:05 -0800

    removed java prfliler files beacuse i was dumb

--- jProfiler/javaProfiler/api/samples/platform/app/src/main/java/LoadFactorProbe.java
+++
@@ -1,8 +0,0 @@
-import com.jprofiler.api.probe.embedded.PayloadProbe;
-
-public class LoadFactorProbe extends PayloadProbe {
-    @Override
-    public String getName() {
-        return "Hash map load factors";
-    }
-}
