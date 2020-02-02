89756a90a2499978024f2f48a9221a4b1c466a95
Eric <erve2005@gmail.com>
2020-02-01 16:02:36 -0800

    Shift Test with Launchpad

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.logging.SyncLogger;
 import frc.robot.oi.ControllerDriver;
+import frc.robot.oi.LaunchpadDriver;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Shifter;
 import frc.robot.utilities.Ports;
@@ -24,6 +25,7 @@ public class RobotContainer {
   private SyncLogger logger;
 
   private ControllerDriver controller1;
+  private LaunchpadDriver launchpad;
 
   // public just to make things work
   private PigeonGyro gyro;
@@ -38,6 +40,7 @@ public class RobotContainer {
     logger = new SyncLogger();
 
     controller1 = new ControllerDriver(logger);
+    launchpad = new LaunchpadDriver(logger);
 
     gyro = new PigeonGyro(Ports.PIGEON_IMU.port);
     drivetrain = new Drivetrain();
@@ -61,6 +64,7 @@ public class RobotContainer {
     controller1.leftBumper.whenPressed(new Shift(shifter));
     controller1.dPadRight.whenPressed(new GyroTurn(gyro, drivetrain, 90));
     controller1.dPadLeft.whenPressed(new GyroTurn(gyro, drivetrain, -90));
+    launchpad.buttonA.whenPressed(new Shift(shifter));
   }
 
   /**
