1ff214bca4dbd2938ba60fa7bf1a93155995807c
Owen <owen356wh@gmail.com>
2020-02-04 17:18:58 -0800

    fixed line spacing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,60 +11,61 @@ import frc.robot.commands.*;
 import frc.robot.devices.PigeonGyro;
 
 /**
- * This class is where the bulk of the robot should be declared.  Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls).  Instead, the structure of the robot
- * (including subsystems, commands, and button mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
 
-  private CommandScheduler scheduler;
+	private CommandScheduler scheduler;
 
-  private SyncLogger logger;
+	private SyncLogger logger;
 
-  private ControllerDriver controller1;
+	private ControllerDriver controller1;
 
-  // public just to make things work
-  private PigeonGyro gyro;
-  private Drivetrain drivetrain;
-  private Shifter shifter;
+	// public just to make things work
+	private PigeonGyro gyro;
+	private Drivetrain drivetrain;
+	private Shifter shifter;
 
-  /**
-   * The container for the robot.  Contains subsystems, OI devices, and commands.
-   */
-  public RobotContainer() {
-    scheduler = CommandScheduler.getInstance();
-    logger = new SyncLogger();
+	/**
+	 * The container for the robot. Contains subsystems, OI devices, and commands.
+	 */
+	public RobotContainer() {
+		scheduler = CommandScheduler.getInstance();
+		logger = new SyncLogger();
 
-    controller1 = new ControllerDriver(logger);
+		controller1 = new ControllerDriver(logger);
 
-    gyro = new PigeonGyro(Ports.PIGEON_IMU.port);
-    drivetrain = new Drivetrain();
-    shifter = new Shifter();
-    
-    scheduler.setDefaultCommand(drivetrain, new ArcadeDrive(drivetrain, controller1, shifter));
+		gyro = new PigeonGyro(Ports.PIGEON_IMU.port);
+		drivetrain = new Drivetrain();
+		shifter = new Shifter();
 
-    logger.addElements(drivetrain, gyro, shifter);
-    scheduler.setDefaultCommand(logger, logger);
+		scheduler.setDefaultCommand(drivetrain, new ArcadeDrive(drivetrain, controller1, shifter));
 
-    configureButtonBindings();
-  }
+		logger.addElements(drivetrain, gyro, shifter);
+		scheduler.setDefaultCommand(logger, logger);
 
-  /**
-   * Use this method to define your button->command mappings.  Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
-   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {
-  }
+		configureButtonBindings();
+	}
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    return null;
-  }
+	/**
+	 * Use this method to define your button->command mappings. Buttons can be
+	 * created by instantiating a {@link GenericHID} or one of its subclasses
+	 * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
+	 * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+	 */
+	private void configureButtonBindings() {
+	}
+
+	/**
+	 * Use this to pass the autonomous command to the main {@link Robot} class.
+	 *
+	 * @return the command to run in autonomous
+	 */
+	public Command getAutonomousCommand() {
+		return null;
+	}
 }
--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -14,80 +14,79 @@ import frc.robot.oi.ControllerDriver;
 
 public class ArcadeDrive extends CommandBase {
 
-  private Drivetrain drivetrain;
-  private ControllerDriver controller;
-  private Shifter shift;
-  private final double deadzone = .1;
+    private Drivetrain drivetrain;
+    private ControllerDriver controller;
+    private Shifter shift;
+    private final double deadzone = .1;
 
-  private double old = 0;
-  //max amount motors can change per 20 ms
-  private final double max_change_rate = .15;
+    private double old = 0;
+    // max amount motors can change per 20 ms
+    private final double max_change_rate = .15;
 
-  /**
-   * teleop driver control
-   * @param drivetrain drivetrain instance
-   * @param controller controller instance
-   * @param shift shifter instance
-   */
-  public ArcadeDrive(
-    Drivetrain drivetrain, ControllerDriver controller, Shifter shift) {
-    this.drivetrain = drivetrain;
-    this.controller = controller;
-    this.shift = shift;
+    /**
+     * teleop driver control
+     * 
+     * @param drivetrain drivetrain instance
+     * @param controller controller instance
+     * @param shift      shifter instance
+     */
+    public ArcadeDrive(Drivetrain drivetrain, ControllerDriver controller, Shifter shift) {
+        this.drivetrain = drivetrain;
+        this.controller = controller;
+        this.shift = shift;
 
-    addRequirements(drivetrain);
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    drivetrain.setOpenRampRate(0);
-    shift.Shift(true);
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-
-    double power = controller.rightTrigger() - controller.leftTrigger();
-
-    double turn = controller.leftStickX();
-
-    //turn deadzone
-    if (Math.abs(turn) < deadzone){
-      turn = 0;
+        addRequirements(drivetrain);
     }
 
-    //power rate of change
-      if(power > old+max_change_rate){
-        power = old+max_change_rate;
-        old = power;
-      }
-      else if(power < old-max_change_rate){
-          power = old-max_change_rate;
-          old = power;
-      }
-      else{
-        old = power;
-      }
-  
-      //calculates power to the motors
-    double leftPower = power + turn;
-    double rightPower = power - turn;
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        drivetrain.setOpenRampRate(0);
+        shift.Shift(true);
+    }
 
-    drivetrain.setLeftMotorPower(leftPower);
-    drivetrain.setRightMotorPower(rightPower);
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
 
-  }
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(final boolean interrupted) {
-    drivetrain.stop();
-  }
+        double power = controller.rightTrigger() - controller.leftTrigger();
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+        double turn = controller.leftStickX();
+
+        // turn deadzone
+        if (Math.abs(turn) < deadzone) {
+            turn = 0;
+        }
+
+        // power rate of change
+        if (power > old + max_change_rate) {
+            power = old + max_change_rate;
+            old = power;
+        } else if (power < old - max_change_rate) {
+            power = old - max_change_rate;
+            old = power;
+        } else {
+            old = power;
+        }
+
+        // calculates power to the motors
+        double leftPower = power + turn;
+        double rightPower = power - turn;
+
+        drivetrain.setLeftMotorPower(leftPower);
+        drivetrain.setRightMotorPower(rightPower);
+
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(final boolean interrupted) {
+        drivetrain.stop();
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
