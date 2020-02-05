b6c9bc22e448011ffb82e0c552c63617de6fdffb
Owen <owen356wh@gmail.com>
2020-02-05 10:32:46 -0800

    stuff I forgot

--- src/main/java/frc/robot/oi/JoystickDriver.java
+++ src/main/java/frc/robot/oi/JoystickDriver.java
@@ -16,22 +16,22 @@ public class JoystickDriver extends GenericDriver implements Logger {
 	button4,
 	button5;
 
+	public LoggerAxis
+	axisX,
+	axisY;
+
 	public JoystickDriver(SyncLogger logger) {
 		button1 = new LoggerButton(getButtonGetter(1), LoggerRelations.PLACEHOLDER, logger);
 		button2 = new LoggerButton(getButtonGetter(2), LoggerRelations.PLACEHOLDER, logger);
 		button3 = new LoggerButton(getButtonGetter(3), LoggerRelations.PLACEHOLDER, logger);
 		button4 = new LoggerButton(getButtonGetter(4), LoggerRelations.PLACEHOLDER, logger);
 		button5 = new LoggerButton(getButtonGetter(5), LoggerRelations.PLACEHOLDER, logger);
+
+		//TODO - make sure axes ports are correct
+		axisX = new LoggerAxis(getAxisGetter(1), LoggerRelations.PLACEHOLDER, logger);
+		axisY = new LoggerAxis(getAxisGetter(2), LoggerRelations.PLACEHOLDER, logger)
 	}
 
-	//TODO - make sure axes ports are correct
-	public double getAxisX() {
-		return getRawAxis(1);
-	}
-
-	public double getAxisY() {
-		return getRawAxis(2);
-	}
 
 	@Override
 	public double[] getValues(double[] values) {
