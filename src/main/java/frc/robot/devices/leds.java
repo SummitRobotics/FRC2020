f3f2549f14ba085cb398a00411f788103a9c1169
Django <django@inventobot.com>
2020-02-05 21:33:04 -0800

    mede if fancy with lambdas

--- src/main/java/frc/robot/devices/leds.java
+++ src/main/java/frc/robot/devices/leds.java
@@ -12,8 +12,10 @@ import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 /**
  * Add your docs here.
  */
-public class leds {
+public class leds{
+    //pwm port the strip is pluged into
     private final int port = 9;
+    //the length of the
     private final int length = 58;
     private AddressableLED ledStrip;
     private AddressableLEDBuffer buffer;
@@ -25,10 +27,58 @@ public class leds {
         ledStrip.start();
     }
 
-    public void makeledscolor(int r, int g, int b){
-    for(int i = 1; i<length; i++){
-        buffer.setRGB(i, 255, 0, 0);
+    /**
+     * changes a range of leds
+     * @param r red 0-255
+     * @param g green 0-255
+     * @param b blue 0-255
+     * @param start led to start change at
+     * @param end led to end change at
+     */
+    public void changeLedRange(int r, int g, int b, int start, int end){
+        for(int i = start; i<=end; i++){
+            buffer.setRGB(i, r, g, b);
+        }
+        ledStrip.setData(buffer);
     }
-    ledStrip.setData(buffer);
+
+    /**
+     * gets a controler to controle a range of leds
+     * @param start led to start at
+     * @param end led to end at
+     * @return usable led range controler
+     */
+    public ledRange getRangeControler(int start, int end){
+        return (int r, int g, int b) -> changeLedRange(r, g, b, start, end);
+    }
+
+    /**
+     * used to group multipul led ranges into a single range
+     * @param ranges the ranges to be grouped
+     * @return the new grouped controler
+     */
+    public ledRange groupRangeControlers(ledRange... ranges){
+        return (int r, int g, int b) -> setMultipulRanges(r, g, b, ranges);
+    }
+
+    /**
+     * used to set multipul ranges at a time
+     * @param r red 0-255
+     * @param g green 0-255
+     * @param b blue 0-255
+     * @param ranges the ranges to be set
+     */
+    private void setMultipulRanges(int r, int g, int b, ledRange... ranges){
+        for(ledRange x : ranges){
+            x.setColor(r, g, b);
+        }
+    }
+
+
+    /**
+     * interface of a led range controler
+     */
+    public interface ledRange {
+        public void setColor(int r, int g, int b);
     }
 }
